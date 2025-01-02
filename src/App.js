65502328e1030fd05420d87a3f4f97b4a6ae80f2
rayan2125 <dhananjay.s.dubey20125@gmail.com>
2025-01-02 22:41:20 +0530

    todays changes

--- src/App.js
+++ src/App.js
@@ -1,86 +1,65 @@
-import { View, Text, StatusBar, useColorScheme } from 'react-native'
-import React, { useState } from 'react'
-import ScreenWrapper from './components/screenWrapper';
+import React, { useState, useEffect } from 'react';
+import { View, Text, StatusBar, useColorScheme } from 'react-native';
 import { Provider } from 'react-redux';
+import ScreenWrapper from './components/screenWrapper';
 import AppStack from './navigations';
 import { store } from './redux/store';
 import Colors from './screens/utility/color';
-import { OSNotification, OneSignal } from 'react-native-onesignal';
+import { OneSignal } from 'react-native-onesignal';
+import messaging from '@react-native-firebase/messaging';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+ 
 
 const App = () => {
   const isDarkMode = useColorScheme() === 'dark';
-//   if (!firebase.apps.length) {
-//     firebase.initializeApp();
-// } 
-console.log("testing",OneSignal.Notifications)
-const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-}; 
-  // const requestUserPermission = async () => { 
-  //   const authStatus = await messaging().requestPermission();
-  //   return (
-  //       authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
-  //       authStatus === messaging.AuthorizationStatus.PROVISIONAL
-  //   );
-  // };  
-  const [nottificationdata,setnottificationdata] = useState('') 
-const [sendnotidata, setsendnotidata] = useState('');
+    useEffect(()=>{
+      OneSignal.initialize('c23f18fe-b63d-4266-8504-5e2a0db11aa5');
+    },[])
+  const [notificationData, setNotificationData] = useState('');
+  // console.log(OneSignal)
+// console.log(OneSignal.setNotificationWillShowInForegroundHandler())
   // useEffect(() => {
-  //   OneSignal.setAppId('c23f18fe-b63d-4266-8504-5e2a0db11aa5');
-  //   OneSignal.promptForPushNotificationsWithUserResponse();
+  //   // Initialize OneSignal
+  //   OneSignal.initialize('c23f18fe-b63d-4266-8504-5e2a0db11aa5');
+
+  //   // Handle foreground notifications
   //   OneSignal.setNotificationWillShowInForegroundHandler(notificationReceivedEvent => {
-  //       let notification = notificationReceivedEvent.getNotification();
-  //       setsendnotidata(notification)
-  //       const data = notification.additionalData;
-  //       setnottificationdata(notification);
-         
-  //       if (data.orderCode) {
-  //           console.log('jnkjnnwihui2n');
-  //           console.log("notification", notification)
-  //           // AsyncStorage.setItem('Notification_data', ''+JSON.stringify(notification));
-  //           // handleNotificationClick("Chat")
-  //       } else if (data.notificationType == '') {
-
-  //       }
-  //       notificationReceivedEvent.complete(notification);
+  //     const notification = notificationReceivedEvent.getNotification();
+  //     console.log('Foreground notification:', notification);
+  //     setNotificationData(notification);
+  //     notificationReceivedEvent.complete(notification);
   //   });
- 
-  
-    
-  //   OneSignal.addSubscriptionObserver(event => {
-  //       console.log('Subscription state changed:', event); 
-  //       if(event.to.userId){
-  //           console.log('onesignal-------token-----------', event.to.userId);  
-  //           AsyncStorage.setItem("onesignaltoken", event.to.userId); 
-  //       }
-  //   });
-  //   const deviceState = (OneSignal.getDeviceState());
-  //   deviceState
-  //   .then((data) => {
-  //       console.log('onesignal-------token-----------', data.userId);
-  //       AsyncStorage.setItem("onesignaltoken", data.userId);
-  //   })
-  //   .catch(error=>{
-  //       console.log("error in getting onesignal token", error);
-  //   })
 
+  //   // Handle clicked notifications
+  //   OneSignal.setNotificationOpenedHandler(notification => {
+  //     console.log('Notification clicked:', notification);
+  //   });
+
+  //   // Retrieve OneSignal token
+  //   OneSignal.getDeviceState()
+  //     .then(state => {
+  //       if (state.userId) {
+  //         console.log('OneSignal Token:', state.userId);
+  //         AsyncStorage.setItem('onesignalToken', state.userId);
+  //       }
+  //     })
+  //     .catch(error => {
+  //       console.error('Error fetching OneSignal token:', error);
+  //     });
+
+  //   // Example usage of tags
+  //   OneSignal.User.addTag('app_user', 'true');
+  //   OneSignal.User.getTags().then(tags => console.log('User Tags:', tags));
   // }, []);
+
   return (
-    
-     
-      <ScreenWrapper>
-        <StatusBar animated={true} backgroundColor="#002F5B" />
-        {/* <Notificationdata.Provider value={{value:nottificationdata}}>  */}
-            
-            <Provider store={store}> 
-                <AppStack/>  
-            </Provider>
-
-        {/* </Notificationdata.Provider> */}
-      </ScreenWrapper> 
-    
+    <ScreenWrapper>
+      <StatusBar animated={true} backgroundColor="#002F5B" />
+      <Provider store={store}>
+        <AppStack />
+      </Provider>
+    </ScreenWrapper>
   );
-  
-}
+};
 
-export default App
+export default App;
